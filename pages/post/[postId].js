daba8286e3318326977b7db4cee826f896e81cda
Dmitry Nastich Usov <dim@usov.mobi>
2024-08-29 14:14:57 -0700

    first commit

---
+++ pages/post/[postId].js
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>The post page</h1>
+    </div>
+  );
+}
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+  return (
+    <div>
+      <h1>The token topup page</h1>
+    </div>
+  );
+}
