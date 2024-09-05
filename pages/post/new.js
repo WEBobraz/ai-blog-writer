6532b8930abe52947ca11d27e0c5878ed77cae2d
Dmitry Nastich Usov <dim@usov.mobi>
2024-09-05 12:09:21 -0700

    Call OpenAI API

--- pages/post/new.js
+++ pages/post/new.js
@@ -2,9 +2,19 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import AppLayout from "../../components/AppLayout/AppLayout";
 
 export default function NewPost(props) {
+  const handleClick = async () => {
+    const response = await fetch(`/api/generatePost`, {
+      method: "POST",
+    });
+    const json = await response.json();
+    console.log("RESULT:", json);
+  };
   return (
     <div>
       <h1>The new post page</h1>
+      <button className="btn" onClick={handleClick}>
+        Generate
+      </button>
     </div>
   );
 }
