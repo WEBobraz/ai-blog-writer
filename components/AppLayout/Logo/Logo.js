16fcf51fe9a416c31edf6f68d89b85e0966cccc6
Dmitry Nastich Usov <dim@usov.mobi>
2024-09-10 18:42:06 -0700

    start utils

--- components/AppLayout/Logo/Logo.js
+++ components/AppLayout/Logo/Logo.js
@@ -1,3 +1,9 @@
+import {
+  faHandshake,
+  faPencil,
+  faPencilAlt,
+  faPencilSquare,
+} from "@fortawesome/free-solid-svg-icons";
 import { faBrain } from "@fortawesome/free-solid-svg-icons/faBrain";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -5,7 +11,7 @@ export const Logo = () => {
   return (
     <div className="text-3xl text-center py-4 font-heading">
       Your AI Writer &nbsp;
-      <FontAwesomeIcon icon={faBrain} className="text-2xl text-slate-400" />
+      <FontAwesomeIcon icon={faPencilAlt} className="text-2xl text-slate-400" />
     </div>
   );
 };
